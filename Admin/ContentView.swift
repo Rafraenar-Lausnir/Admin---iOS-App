1217dcd10ceb3b284e9756bfe983c8f7fa8eac9a
Elín Ósk <elinosk@elinosk.com>
2024-08-14 17:47:15 +0000

    [Feature] Updated icon and text

--- Admin/ContentView.swift
+++ Admin/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "heart.fill")
+        .font(.largeTitle)
+        .foregroundStyle(.tint)
+      Text("Rafrænar Lausnir!")
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
