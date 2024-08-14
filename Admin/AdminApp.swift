34e3a353bc97a9e80494e029381a68179e5d411e
Elín Ósk <elinosk@elinosk.com>
2024-08-14 16:41:12 +0000

    Initial Commit

---
+++ Admin/AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdminApp.swift
+//  Admin
+//
+//  Created by Elín Ósk on 14.8.2024.
+//
+
+import SwiftUI
+
+@main
+struct AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
