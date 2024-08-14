354745514eafb9394cc7ae73bfe3081b7405667f
Elín Ósk <elinosk@elinosk.com>
2024-08-14 22:03:43 +0000

    [Feature] Sign in button added

--- Admin/ContentView.swift
+++ Admin/ContentView.swift
@@ -9,22 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      Image("Logo_Icon")
-        .resizable()
-        .frame(width: 150, height: 150)
-      Text("Rafrænar Lausnir")
-        .font(.title)
-        .foregroundStyle(Color("text_color"))
-      Text("Rafrænt handa öllum!")
-        .font(.headline)
-        .foregroundStyle(Color("text_color"))
+    NavigationStack {
+      VStack {
+        Image("Logo_Icon")
+          .resizable()
+          .frame(width: 150, height: 150)
+        Text("Rafrænar Lausnir")
+          .font(.title)
+          .foregroundStyle(Color("text_color"))
+        Text("Rafrænt handa öllum!")
+          .font(.headline)
+          .foregroundStyle(Color("text_color"))
+
+        Spacer()
+
+        NavigationLink {
+          // Authentication screen
+        } label: {
+          Text("Skrá inn")
+            .padding()
+            .padding(.horizontal)
+            .foregroundStyle(Color("bg_color"))
+            .bold()
+            .background(Color("text_color"))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+
+      }
+      .padding()
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(
+        Color("bg_color")
+      )
     }
-    .padding()
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(
-      Color("bg_color")
-    )
   }
 }
 
